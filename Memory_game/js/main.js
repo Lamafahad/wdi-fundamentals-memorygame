3a50c7c971143cd90f22cab7e70098bc6057ec08
Lama Fahad <lamafahad@Lamas-MacBook-Air-2.local>
2019-09-25 02:40:45 +0300

    created game logic

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -1,8 +1,23 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
+
+
+const cards = ["queen" , "queen" , "king" , "king"];
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
+console.log("User flipped king");
+
+if (cardsInPlay.length !== 2){
+  alert ("select two cards please!");
+}
+   else if (cardOne === cardTwo){
+  alert("You found a match!");
+}
+else {
+alert("Sorry, Try again"); }
