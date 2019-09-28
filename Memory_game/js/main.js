3c92e0d321e74e6282ad2957e499a63f686d3ad4
Lama Fahad <lamafahad@Lamas-MacBook-Air-2.local>
2019-09-29 00:38:40 +0300

    created game logic

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -35,13 +35,29 @@ function checkForMatch(){
 }
 };
 
-function flipCard(cardId) {
+
+function createBoard(){
+
+  for (var i = 0; i < cards.length; i++) {
+    const cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+      // Logic here
+  }
+
+}
+
+function flipCard() {
+const cardId = this.getAttribute('data-id');
 
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
-
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+
+  this.setAttribute('src', cards[cardId].cardImage);
 
   if (cardsInPlay.length !== 2){
     alert ("select two cards please!"); }
@@ -50,5 +66,13 @@ function flipCard(cardId) {
     }
   };
 
-flipCard(0);
-flipCard(2);
+/* function createBoard()
+  for (var i = 0; i < cards.length; i++)
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-game-board', cards[i]);
+    cardElement.addEventListener('click' , flipCard );
+    document.getElementById('game-board').appendChild(cardElement);
+ */
+
+createBoard();
